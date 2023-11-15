9c7c10cb12c2765d8632ffa088ae3b472e9838b6
Rea Fernandes <150733676+reafernandes10@users.noreply.github.com>
2023-11-15 11:52:24 +0100

    Update App.js
    Adding a welcome message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import "./App.css";
 import Game from "./Game/Game";
 
 function App() {
-  return <Game />;
+  let wel = "Welcome to TicTacToe!";
+  return (
+    <>
+    <div className="wel">{wel}</div>
+    <Game />
+    </>
+    );
 }
 
 export default App;
