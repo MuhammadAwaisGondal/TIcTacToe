661bd686a951c11f66bf8a651b646b46f4bffe25
Nivedha <nivedhasrinivasan091199@gmail.com>
2023-11-16 17:10:47 +0100

    Added move function

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -70,7 +70,17 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move) => {
-    // TODO: Implement this function
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
   });
 
   return (
