cf04969da47ef7b5627905294fd769211c5f3498
Rea Fernandes <150733676+reafernandes10@users.noreply.github.com>
2023-11-27 23:23:53 +0000

    1. Removing inline coding for sun and moon colors and moving it .css 2. Removing variable from welcome message.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import Game from "./Game/Game";
 import { useState } from "react";
 
 function App() {
-  let wel = "Welcome to TicTacToe!";
   const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
       <div className="container">
-        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <span className={`sun ${darkMode ? 'dark-mode' : ''}`}>☀︎</span>
         <div className="switch-checkbox">
           <label className="switch">
             <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
             <span className="slider round"> </span>
           </label>
         </div>
-        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+        <span className={`moon ${darkMode ? 'dark-mode' : ''}`}>☽</span>
       </div>
       <div>
-        <h1 className="welcome">{wel}</h1>
+        <h1 className="welcome">"Welcome to TicTacToe!"</h1>
         <Game />
       </div>
     </div>
