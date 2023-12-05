16a41de97d61b035339d2c5d399a0504b82022c4
Rea Fernandes <150733676+reafernandes10@users.noreply.github.com>
2023-12-05 20:18:53 +0000

    1. Switch Players During the Game 2. Game Result and Reset 3. Input Validation for Player Names

--- src/App.test.js
+++ src/App.test.js
@@ -64,3 +64,57 @@ test('Make a Move in the Game', () => {
   expect(statusElement).toBeInTheDocument();
   expect(moveElement).toBeInTheDocument();
 });
+
+test('Switch Players During the Game', () => {
+  const { getByText, getByTestId } = render(<App />);
+  const switchElement = getByText('☀︎');
+  fireEvent.click(switchElement); // Toggle to dark mode
+
+  // Make moves and check player alternation
+  const square0 = getByTestId('square-0');
+  const square1 = getByTestId('square-1');
+  fireEvent.click(square0);
+  expect(getByText('Next player:')).toHaveTextContent('Next player: Player O');
+  fireEvent.click(square1);
+  expect(getByText('Next player:')).toHaveTextContent('Next player: Player X');
+});
+
+test('Game Result and Reset', () => {
+  const { getByText, getByTestId } = render(<App />);
+  const switchElement = getByText('☀︎');
+  fireEvent.click(switchElement); // Toggle to dark mode
+
+  // Play through a complete game
+  const squares = Array.from({ length: 9 }, (_, i) => getByTestId(`square-${i}`));
+  fireEvent.click(squares[0]);
+  fireEvent.click(squares[3]);
+  fireEvent.click(squares[1]);
+  fireEvent.click(squares[4]);
+  fireEvent.click(squares[2]);
+
+  // Check the game result
+  expect(getByText('Game Result: Player X Wins!')).toBeInTheDocument();
+
+  // Reset the game
+  const resetButton = getByText('Go to game start');
+  fireEvent.click(resetButton);
+
+  // Check that the game is reset
+  expect(getByText('Next player:')).toHaveTextContent('Next player: Player X');
+});
+
+test('Input Validation for Player Names', () => {
+  const { getByText, getByLabelText } = render(<App />);
+  const switchElement = getByText('☀︎');
+  fireEvent.click(switchElement); // Toggle to dark mode
+
+  // Input invalid player names
+  const player1Input = getByLabelText('Player 1:');
+  const player2Input = getByLabelText('Player 2:');
+  fireEvent.change(player1Input, { target: { value: '' } });
+  fireEvent.change(player2Input, { target: { value: 'NameExceedingLimit' } });
+
+  // Check for validation messages
+  expect(getByText('Please enter a valid name for Player 1')).toBeInTheDocument();
+  expect(getByText('Player 2 name should not exceed 10 characters')).toBeInTheDocument();
+});
